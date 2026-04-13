544c9951ebb6045e4eb96b0de4821da288dfe181
Гвоздев Александр <aleksandr.gvozdev@gazprombank.ru>
2026-04-13 18:59:36 +0300

    13.04.2026

--- src/main/java/com/pluxurydolo/telegram/client/TelegramTextClient.java
+++ src/main/java/com/pluxurydolo/telegram/client/TelegramTextClient.java
@@ -1,29 +1,58 @@
 package com.pluxurydolo.telegram.client;
 
 import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.request.EditMessageText;
 import com.pengrad.telegrambot.request.SendMessage;
+import com.pluxurydolo.telegram.dto.request.SendEditTextRequest;
 import com.pluxurydolo.telegram.dto.request.SendTextRequest;
+import com.pluxurydolo.telegram.exception.SendEditTextException;
+import com.pluxurydolo.telegram.exception.SendTextException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
 import static com.pengrad.telegrambot.model.request.ParseMode.HTML;
 
 public class TelegramTextClient {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TelegramTextClient.class);
+
     private final long userId;
 
     public TelegramTextClient(long userId) {
         this.userId = userId;
     }
 
-    public Mono<String> sendText(SendTextRequest sendTextRequest) {
-        String text = sendTextRequest.text();
-        TelegramBot bot = sendTextRequest.bot();
+    public Mono<Integer> sendText(SendTextRequest request) {
+        String text = request.text();
+        TelegramBot bot = request.bot();
 
-        SendMessage request = new SendMessage(userId, text)
+        SendMessage sendMessage = new SendMessage(userId, text)
             .parseMode(HTML);
 
-        return Mono.fromCallable(() -> bot.execute(request))
+        return Mono.fromCallable(() -> bot.execute(sendMessage))
+            .map(response -> response.message().messageId())
+            .onErrorResume(throwable -> {
+                LOGGER.error("rghu [telegram-starter] Произошла ошибка при редактировании сообщения с текстом {}", text);
+                return Mono.error(new SendTextException(throwable));
+            })
+            .subscribeOn(Schedulers.boundedElastic());
+    }
+
+    public Mono<String> sendEditText(SendEditTextRequest request) {
+        String text = request.text();
+        int messageId = request.messageId();
+        TelegramBot bot = request.bot();
+
+        EditMessageText editMessageText = new EditMessageText(userId, messageId, text)
+            .parseMode(HTML);
+
+        return Mono.fromCallable(() -> bot.execute(editMessageText))
             .thenReturn(text)
+            .onErrorResume(throwable -> {
+                LOGGER.error("okhw [telegram-starter] Произошла ошибка при редактировании сообщения с текстом {}", text);
+                return Mono.error(new SendEditTextException(throwable));
+            })
             .subscribeOn(Schedulers.boundedElastic());
     }
 }
--- src/test/java/com/pluxurydolo/telegram/client/TelegramTextClientTests.java
+++ src/test/java/com/pluxurydolo/telegram/client/TelegramTextClientTests.java
@@ -1,7 +1,9 @@
 package com.pluxurydolo.telegram.client;
 
 import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.response.SendResponse;
+import com.pluxurydolo.telegram.dto.request.SendEditTextRequest;
 import com.pluxurydolo.telegram.dto.request.SendTextRequest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,6 +12,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import reactor.core.publisher.Mono;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 import static reactor.test.StepVerifier.create;
 
@@ -23,20 +26,66 @@ class TelegramTextClientTests {
     @Mock
     private SendResponse sendResponse;
 
+    @Mock
+    private Message message;
+
     @Test
     void testSendText() {
         when(telegramBot.execute(any()))
             .thenReturn(sendResponse);
+        when(sendResponse.message())
+            .thenReturn(message);
+        when(message.messageId())
+            .thenReturn(1);
 
-        Mono<String> result = CLIENT.sendText(sendTextRequest(telegramBot));
+        Mono<Integer> result = CLIENT.sendText(sendTextRequest(telegramBot));
+
+        create(result)
+            .expectNext(1)
+            .verifyComplete();
+    }
+
+    @Test
+    void testSendTextWhenExceptionOccurred() {
+        doThrow(RuntimeException.class)
+            .when(telegramBot).execute(any());
+
+        Mono<Integer> result = CLIENT.sendText(sendTextRequest(telegramBot));
+
+        create(result)
+            .expectError(RuntimeException.class)
+            .verify();
+    }
+
+    @Test
+    void testSendEditText() {
+        when(telegramBot.execute(any()))
+            .thenReturn(sendResponse);
+
+        Mono<String> result = CLIENT.sendEditText(sendEditTextRequest(telegramBot));
 
         create(result)
             .expectNext("text")
             .verifyComplete();
     }
 
+    @Test
+    void testSendEditTextWhenExceptionOccurred() {
+        doThrow(RuntimeException.class)
+            .when(telegramBot).execute(any());
+
+        Mono<String> result = CLIENT.sendEditText(sendEditTextRequest(telegramBot));
+
+        create(result)
+            .expectError(RuntimeException.class)
+            .verify();
+    }
+
     private static SendTextRequest sendTextRequest(TelegramBot telegramBot) {
-        String text = "text";
-        return new SendTextRequest(text, telegramBot);
+        return new SendTextRequest("text", telegramBot);
+    }
+
+    private static SendEditTextRequest sendEditTextRequest(TelegramBot telegramBot) {
+        return new SendEditTextRequest("text", 1, telegramBot);
     }
 }
