20f45c5895538c18a55b00b251bd57bba353df2f
Гвоздев Александр <aleksandr.gvozdev@gazprombank.ru>
2026-05-10 14:41:52 +0300

    10.05.2026

---
+++ src/test/java/com/pluxurydolo/telegram/handler/text/AbstractTextHandlerTests.java
@@ -0,0 +1,93 @@
+package com.pluxurydolo.telegram.handler.text;
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.model.Message;
+import com.pengrad.telegrambot.model.Update;
+import com.pluxurydolo.telegram.client.TelegramTextClient;
+import com.pluxurydolo.telegram.dto.UpdateType;
+import com.pluxurydolo.telegram.ratelimiter.PerUserRateLimiter;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+
+import static com.pluxurydolo.telegram.dto.UpdateType.TEXT;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class AbstractTextHandlerTests {
+    private static final TelegramTextClient TELEGRAM_TEXT_CLIENT = mock(TelegramTextClient.class);
+    private static final PerUserRateLimiter PER_USER_RATE_LIMITER = mock(PerUserRateLimiter.class);
+    private static final Update UPDATE = mock(Update.class);
+    private static final Message MESSAGE = mock(Message.class);
+
+    @Test
+    void testUpdateType() {
+        UpdateType result = textHandler.updateType();
+
+        assertThat(result)
+            .isEqualTo(TEXT);
+    }
+
+    @Test
+    void testCondition() {
+        when(UPDATE.message())
+            .thenReturn(MESSAGE);
+        when(MESSAGE.text())
+            .thenReturn("text");
+
+        boolean result = textHandler.condition(UPDATE);
+
+        assertThat(result)
+            .isTrue();
+    }
+
+    @Test
+    void testConditionWhenMessageIsNull() {
+        when(UPDATE.message())
+            .thenReturn(null);
+
+        boolean result = textHandler.condition(UPDATE);
+
+        assertThat(result)
+            .isFalse();
+    }
+
+    @Test
+    void testConditionWhenMessageTextIsCommand() {
+        when(UPDATE.message())
+            .thenReturn(MESSAGE);
+        when(MESSAGE.text())
+            .thenReturn("/text");
+
+        boolean result = textHandler.condition(UPDATE);
+
+        assertThat(result)
+            .isFalse();
+    }
+
+    private AbstractTextHandler textHandler = new AbstractTextHandler(
+        TELEGRAM_TEXT_CLIENT,
+        PER_USER_RATE_LIMITER
+    ) {
+
+        @Override
+        public Mono<String> doWork(Update u) {
+            return null;
+        }
+
+        @Override
+        public String failMessage() {
+            return null;
+        }
+
+        @Override
+        public TelegramBot telegramBot() {
+            return null;
+        }
+
+        @Override
+        public Mono<String> onException(Throwable throwable) {
+            return null;
+        }
+    };
+}
