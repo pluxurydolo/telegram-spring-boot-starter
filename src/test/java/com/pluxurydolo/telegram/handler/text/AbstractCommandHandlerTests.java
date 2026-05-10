551c07789d41399aa93b191c3ff7b75a4a40cc28
Гвоздев Александр <aleksandr.gvozdev@gazprombank.ru>
2026-05-10 14:39:37 +0300

    10.05.2026

---
+++ src/test/java/com/pluxurydolo/telegram/handler/text/AbstractCommandHandlerTests.java
@@ -0,0 +1,98 @@
+package com.pluxurydolo.telegram.handler.text;
+
+import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.model.Message;
+import com.pengrad.telegrambot.model.Update;
+import com.pluxurydolo.telegram.client.TelegramTextClient;
+import com.pluxurydolo.telegram.dto.UpdateType;
+import com.pluxurydolo.telegram.ratelimiter.PerUserRateLimiter;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
+
+import static com.pluxurydolo.telegram.dto.UpdateType.COMMAND;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class AbstractCommandHandlerTests {
+    private static final TelegramTextClient TELEGRAM_TEXT_CLIENT = mock(TelegramTextClient.class);
+    private static final PerUserRateLimiter PER_USER_RATE_LIMITER = mock(PerUserRateLimiter.class);
+    private static final Update UPDATE = mock(Update.class);
+    private static final Message MESSAGE = mock(Message.class);
+
+    @Test
+    void testUpdateType() {
+        UpdateType result = commandHandler.updateType();
+
+        assertThat(result)
+            .isEqualTo(COMMAND);
+    }
+
+    @Test
+    void testCondition() {
+        when(UPDATE.message())
+            .thenReturn(MESSAGE);
+        when(MESSAGE.text())
+            .thenReturn("/command1");
+
+        boolean result = commandHandler.condition(UPDATE);
+
+        assertThat(result)
+            .isTrue();
+    }
+
+    @Test
+    void testConditionWhenMessageIsNull() {
+        when(UPDATE.message())
+            .thenReturn(null);
+
+        boolean result = commandHandler.condition(UPDATE);
+
+        assertThat(result)
+            .isFalse();
+    }
+
+    @Test
+    void testConditionWhenMessageTextDoesNotStartWithCommand() {
+        when(UPDATE.message())
+            .thenReturn(MESSAGE);
+        when(MESSAGE.text())
+            .thenReturn("/idk");
+
+        boolean result = commandHandler.condition(UPDATE);
+
+        assertThat(result)
+            .isFalse();
+    }
+
+    private AbstractCommandHandler commandHandler = new AbstractCommandHandler(
+        TELEGRAM_TEXT_CLIENT,
+        PER_USER_RATE_LIMITER
+    ) {
+
+        @Override
+        public String command() {
+            return "/command";
+        }
+
+        @Override
+        public Mono<String> doWork(Update update) {
+            return null;
+        }
+
+        @Override
+        public String failMessage() {
+            return null;
+        }
+
+        @Override
+        public TelegramBot telegramBot() {
+            return null;
+        }
+
+        @Override
+        public Mono<String> onException(Throwable throwable) {
+            return null;
+        }
+    };
+}
